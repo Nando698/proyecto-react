9253f01ec41652429d47ba4517099f39da9d2152
Nando698 <diazfernando07@gmail.com>
2022-03-14 20:28:52 -0300

    cambios esteticos

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,25 +1,30 @@
 import Button from '@mui/material/Button';
 import logo from '../img/logo/perrito.png';
 import './NavBar.css';
+import 'animate.css';
 
 function NavBar() {
   return (
     <div className="navbar">
       <header className="main-header">
         
-        <img src={logo} alt="" className='img-logo' />
+        <img src={logo} alt="" className='img-logo animate__animated animate__backInRight' />
         
         <ul className='menu active'>
           
           <li><Button variant='contained'>Inicio</Button></li>
-          <li><Button>Quienes somos</Button></li>
-          <li><Button>Encontra tu mascota</Button></li>
-          <li><Button>Publica mascota encontrada</Button></li>
+          <li><Button variant='contained'>Quienes somos</Button></li>
+          <li><Button variant='contained'>Encontrá</Button></li>
+          <li><Button variant='contained'>Publicá</Button></li>
 
         </ul>
         
       </header>
     </div>
+
+    
+
+
   );
 }
 
