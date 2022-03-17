af60747ecfe1467295b146057048c89017ea40c8
Nando698 <diazfernando07@gmail.com>
2022-03-16 22:34:02 -0300

    agrego fav, itemcontainer y cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import './App.css';
-import Button from '@mui/material/Button';
+
 
 function App() {
   return (
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,13 +2,13 @@ import './Card.css';
 
 
 
-export default function(props) {
+export default function Card(props) {
     
     return (
 
         <div className='Card'>
             <h2>{props.nombre}</h2>
-            <img src='https://st.depositphotos.com/2398521/2608/i/950/depositphotos_26089317-stock-photo-cute-small-dog.jpg'></img>
+            <img src='https://st.depositphotos.com/2398521/2608/i/950/depositphotos_26089317-stock-photo-cute-small-dog.jpg' alt=""></img>
             <p>Edad: {props.edad}</p>
             <p>Tamaño : {props.tamanio}</p>
             <button> Agregar a favs </button>
